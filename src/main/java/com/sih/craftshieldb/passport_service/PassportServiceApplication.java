7ad2dae6ad144d816bf26f2063328364d001daca
DEEPANKAR BEHERA <deepankarb12@outlook.com>
2026-08-13 22:02:51 +0530

    Spring Boot architecture with Web, JPA, Postgres, and ZXing

---
+++ src/main/java/com/sih/craftshieldb/passport_service/PassportServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sih.craftshieldb.passport_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassportServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassportServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sih/craftshieldb/passport_service/PassportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sih.craftshieldb.passport_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassportServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
